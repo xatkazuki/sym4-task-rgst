3b262259e2c7336d6fbb0d5f940e78b1cae965c7
takabayashi <you@example.com>
2019-04-10 17:37:24 +0900

    php bin/console doctrine:fixtures:load実行してdummyでユーザ情報登録 "dummy@exmample","testpass"

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\User;
 
 class UserFixture extends Fixture
 {
@@ -11,6 +12,12 @@ class UserFixture extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+      $user = new User();
+      $user-> setEmail('dummy@example.com');
+      $user-> setPassword('$2y$12$mSOv9PV3JNZl.BpzoBS38.KWfG4g3lHyLVYcYzHUpkzElHP1YLxY6');
+//      $product->setPrice(14.50);
+//      $product->setDescription('Ok, I guess it *does* have a price');
+      $manager->persist($user);
 
         $manager->flush();
     }
