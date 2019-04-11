7e1f9e02554e4b0d9b9fbbec7affb3a0ec496b06
takabayashi <you@example.com>
2019-04-11 19:31:25 +0900

    リダイレクト先を修正

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -87,7 +87,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
            // redirect to some "app_homepage" route - of wherever you want
-           return new RedirectResponse($this->urlGenerator->generate('app_homepage'));
+           return new RedirectResponse($this->urlGenerator->generate('admin'));
 
     }
 
