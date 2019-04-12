f5f74014bd289d45b75660a968ddca7ed8ba42ff
takabayashi <you@example.com>
2019-04-13 06:58:25 +0900

    formを作成、registとshowを作成

--- src/Controller/Admin/AdminMemberRegistController.php
+++ src/Controller/Admin/AdminMemberRegistController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
+use App\Form\MemberType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminMemberRegistController extends AbstractController
@@ -10,10 +15,31 @@ class AdminMemberRegistController extends AbstractController
     /**
      * @Route("/admin/member/regist", name="admin_memmer_regist")
      */
-    public function index()
+    public function index(Request $request)
     {
-        return $this->render('admin/member-regist.html.twig', [
-            'controller_name' => 'AdminMemberRegistController',
+
+      $member = new User();
+      $member->setCreatedAt(new \DateTime());
+      $member->setUpdatedAt(new \DateTime());
+
+      $form = $this->createForm(MemberType::class,$member);
+//      $form->add('created_at',HiddenType::class,array());
+//      $form->add('updated_at',HiddenType::class,array());
+      $form->add('save', SubmitType::class);
+      $form->handleRequest($request);
+
+      if($form->isSubmitted() && $form->isValid()){
+        $entityManager=$this->getDoctrine()->getManager();
+        $entityManager->persist($member);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('admin_memmer_show');
+      }
+
+
+      return $this->render('admin/member-regist.html.twig', [
+          'controller_name' => 'AdminMemberRegistController',
+          'form' => $form->createView(),
         ]);
     }
   /**
@@ -21,8 +47,14 @@ class AdminMemberRegistController extends AbstractController
    */
   public function show()
   {
+    $entityManager=$this->getDoctrine()->getManager();
+    $memberList = $this->getDoctrine()
+      ->getRepository(User::class)
+      ->findAll();
+
     return $this->render('admin/member-show.html.twig', [
       'controller_name' => 'AdminMemberRegisttController',
+      'MemberList' => $memberList,
     ]);
   }
   /**
---
+++ src/Form/MemberType.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xearts
+ * Date: 2019/04/13
+ * Time: 6:10
+ */
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class MemberType extends AbstractType
+{
+  public function buildForm(FormBuilderInterface $builder, array $options)
+  {
+    $builder
+      ->add('email')
+      ->add('password')
+      ->add('name')
+      ->add('famillyname')
+      ->add('github')
+      ->add('created_at')
+      ->add('updated_at');
+//      ->add('save',SubmitType::class);
+    ;
+  }
+
+}
