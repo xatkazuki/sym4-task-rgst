c823fd3877f66f3a0df6954cbe6f38007b32a697
takabayashi <you@example.com>
2019-04-14 11:38:02 +0900

    datetimeを登録画面では非表示,登録時の入力パスワードをハッシュ化してDBへ保存されるように修正

--- src/Controller/Admin/AdminMemberRegistController.php
+++ src/Controller/Admin/AdminMemberRegistController.php
@@ -4,29 +4,42 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use App\Form\MemberType;
+use PhpParser\Node\Expr\Cast\String_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AdminMemberRegistController extends AbstractController
 {
     /**
      * @Route("/admin/member/regist", name="admin_memmer_regist")
      */
-    public function index(Request $request)
+    public function index(Request $request,UserPasswordEncoderInterface $encoder)
     {
 
       $member = new User();
       $member->setCreatedAt(new \DateTime());
       $member->setUpdatedAt(new \DateTime());
 
+      echo'<pre>';
+      var_dump(new \DateTime());
+      echo'</pre>';
+
       $form = $this->createForm(MemberType::class,$member);
-//      $form->add('created_at',HiddenType::class,array());
-//      $form->add('updated_at',HiddenType::class,array());
+//      $form->add('created_time','datetime',['date_widget' => 'text']);
+//      $form->add('updated_at',,array());
       $form->add('save', SubmitType::class);
       $form->handleRequest($request);
+
+      /**
+       * password入力に関して平文をhashに変換
+       */
+      $plainPassword = $form->get('password')->getData();
+      $encoded = $encoder->encodePassword($member, $plainPassword);
+      $member->setPassword($encoded);
 
       if($form->isSubmitted() && $form->isValid()){
         $entityManager=$this->getDoctrine()->getManager();
@@ -38,8 +51,10 @@ class AdminMemberRegistController extends AbstractController
 
 
       return $this->render('admin/member-regist.html.twig', [
-          'controller_name' => 'AdminMemberRegistController',
-          'form' => $form->createView(),
+        'controller_name' => 'AdminMemberRegistController',
+        'form' => $form->createView(),
+//        'CreatedAt' => $CreatedAt = new \DateTime(),
+//        'UpdatedAt' => $UpdatedAt = new \DateTime(),
         ]);
     }
   /**
@@ -55,6 +70,25 @@ class AdminMemberRegistController extends AbstractController
     return $this->render('admin/member-show.html.twig', [
       'controller_name' => 'AdminMemberRegisttController',
       'MemberList' => $memberList,
+    ]);
+  }
+  /**
+   * @Route("/admin/member/show/{user_name}", name="admin_memmer_show_user", methods={"GET","POST"})
+   */
+  public function showUser(Request $request, $user_name)
+  {
+    $entityManager=$this->getDoctrine()->getManager();
+    $memberData = $this->getDoctrine()
+      ->getRepository(User::class)
+      ->findBy(
+        array(
+          'name' => $user_name
+        )
+      );
+
+    return $this->render('admin/member-show.html.twig', [
+      'controller_name' => 'AdminMemberRegisttController',
+      'MemberList' => $memberData,
     ]);
   }
   /**
--- src/Form/MemberType.php
+++ src/Form/MemberType.php
@@ -9,9 +9,9 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class MemberType extends AbstractType
 {
